72b265be3af32dc3c6f5e27486626dcb4965c572
Stefan Novak <stefannovak96@gmail.com>
2019-06-26 16:16:59 +0100

    Initial Project Add

---
+++ src/main/java/com/stefanalexnovak/snsfgpetclinic/SnSfgPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.stefanalexnovak.snsfgpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnSfgPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnSfgPetClinicApplication.class, args);
+    }
+
+}
