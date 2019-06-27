11f9061b06154acbf1eaa3bb2a67a20f5a7d2d6d
Stefan Novak <stefannovak96@gmail.com>
2019-06-27 16:20:46 +0100

    Added owner controllers and owner index page. Closes #14.

---
+++ pet-clinic-web/src/main/java/com/stefanalexnovak/snsfgpetclinic/controllers/OwnerController.java
@@ -0,0 +1,14 @@
+package com.stefanalexnovak.snsfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"", "/", "/index"})
+    public String listOwners(){
+        return "owners/index";
+    }
+}
