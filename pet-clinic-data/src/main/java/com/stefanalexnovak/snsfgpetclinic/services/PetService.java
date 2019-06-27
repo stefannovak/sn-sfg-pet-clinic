364790b998758c6e029711c9c84198e4fe118d0d
Stefan Novak <stefannovak96@gmail.com>
2019-06-27 15:29:24 +0100

    Refactored to common crud interface. closes #12

--- pet-clinic-data/src/main/java/com/stefanalexnovak/snsfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/stefanalexnovak/snsfgpetclinic/services/PetService.java
@@ -2,13 +2,5 @@ package com.stefanalexnovak.snsfgpetclinic.services;
 
 import com.stefanalexnovak.snsfgpetclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 }
--- pet-clinic-data/src/main/java/com/stefanalexnovak/snsfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/stefanalexnovak/snsfgpetclinic/services/VetService.java
@@ -1,14 +1,5 @@
 package com.stefanalexnovak.snsfgpetclinic.services;
-
 import com.stefanalexnovak.snsfgpetclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 }
