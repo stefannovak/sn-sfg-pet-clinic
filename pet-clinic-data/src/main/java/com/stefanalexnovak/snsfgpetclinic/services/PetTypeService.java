e72785e8d2090f56321a7032804c14dbfccc4227
Stefan Novak <stefannovak96@gmail.com>
2019-06-29 19:13:35 +0100

    added PetTypeService. closes #48

---
+++ pet-clinic-data/src/main/java/com/stefanalexnovak/snsfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.stefanalexnovak.snsfgpetclinic.services;
+
+import com.stefanalexnovak.snsfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
