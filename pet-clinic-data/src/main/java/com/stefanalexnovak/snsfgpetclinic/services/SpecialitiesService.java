39561ec80ef895bcdf9990c46f3b0e0580163b61
Stefan Novak <stefannovak96@gmail.com>
2019-06-30 14:32:54 +0100

    Added specialities Map Impl, closes #40

---
+++ pet-clinic-data/src/main/java/com/stefanalexnovak/snsfgpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package com.stefanalexnovak.snsfgpetclinic.services;
+
+import com.stefanalexnovak.snsfgpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
