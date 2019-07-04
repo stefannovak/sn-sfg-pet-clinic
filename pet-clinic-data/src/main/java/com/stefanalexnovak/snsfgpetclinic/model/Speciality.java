ecfb5e744f93f1b4b57008a77f86415d9f54c867
Stefan Novak <stefannovak96@gmail.com>
2019-07-04 18:08:04 +0100

    Updated vet to use JPA. closes #34.

--- pet-clinic-data/src/main/java/com/stefanalexnovak/snsfgpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/stefanalexnovak/snsfgpetclinic/model/Speciality.java
@@ -1,6 +1,13 @@
 package com.stefanalexnovak.snsfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity {
+    @Column(name = "name")
     private String description;
 
     public String getDescription() {
