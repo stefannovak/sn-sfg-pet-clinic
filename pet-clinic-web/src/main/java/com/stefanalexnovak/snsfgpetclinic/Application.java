59c64c6acc8a128bdd6d55ae042777387be2d192
Stefan Novak <stefannovak96@gmail.com>
2019-07-04 17:53:08 +0100

    Updated base entity with JPA annotations. closes #36.

--- pet-clinic-web/src/main/java/com/stefanalexnovak/snsfgpetclinic/Application.java
+++ pet-clinic-web/src/main/java/com/stefanalexnovak/snsfgpetclinic/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SnSfgPetClinicApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SnSfgPetClinicApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
