e96ac1faea43dbf8e11525fc1b3d40ecc100e417
Stefan Novak <stefannovak96@gmail.com>
2019-07-07 15:06:20 +0100

    Added project Lombok. Might delete it, not sure if I like it. closes #55

--- pet-clinic-data/src/main/java/com/stefanalexnovak/snsfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/stefanalexnovak/snsfgpetclinic/model/PetType.java
@@ -1,19 +1,20 @@
 package com.stefanalexnovak.snsfgpetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "types")
 public class PetType extends BaseEntity {
 
     @Column(name = "name")
     private String name;
-
-    public String getName;
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
--- pet-clinic-data/src/main/java/com/stefanalexnovak/snsfgpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/stefanalexnovak/snsfgpetclinic/model/Speciality.java
@@ -1,20 +1,19 @@
 package com.stefanalexnovak.snsfgpetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "specialities")
 public class Speciality extends BaseEntity {
     @Column(name = "name")
     private String description;
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
